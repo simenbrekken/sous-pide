90dac5ef4d0b339b263be2346dc1fc1574135a02
Simen Brekken <simen@unfold.no>
2013-09-14 05:48:00 +0200

    Refactor and complete timer.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,6 +6,7 @@
     var firebase = new Firebase('https://sous-pide.firebaseio.com'),
         device = firebase.child('devices/simen')
 
+    // Notify agent when data needs to be retrieved via Firebase's REST API
     var notifyAgent = (function() {
         var promise
 
@@ -29,18 +30,23 @@
     // Detect touch
     $('html').toggleClass('no-touch', !('ontouchstart' in document.documentElement))
 
-    // Bound value
-    $('.value[data-path]').each(function() {
-        var $value = $(this)
+    // Reading
+    $('.reading[data-path]').each(function() {
+        var $reading = $(this),
+            $value = $reading.find('.value')
 
-        var path = $value.attr('data-path'),
+        var path = $reading.attr('data-path'),
             format = $value.attr('data-format') || '%.1f',
-            node = device.child(path)
+            node = device.child(path),
+            value
 
         node.on('value', function(snapshot) {
-            var value = +snapshot.val()
+            var previousValue = value
+
+            value = +snapshot.val()
 
             $value.text(sprintf(format, value))
+            $reading.toggleClass('rising', value > previousValue).toggleClass('falling', value < previousValue)
         })
     })
 
@@ -48,7 +54,15 @@
     $('.stepper').each(function() {
         var $stepper = $(this)
 
-        var step = +$stepper.attr('data-step') || 1
+        var path = $stepper.attr('data-path'),
+            node = device.child(path),
+            step = +$stepper.attr('data-step') || 1,
+            value
+
+        node.on('value', function(snapshot) {
+            value = snapshot.val() || 0
+        })
+
         $stepper.on('mousedown touchstart', '.button', function(e) {
             e.preventDefault()
 
@@ -59,9 +73,13 @@
                 timer
 
             function adjust() {
-                var value = (+$stepper.attr('data-value') || 0) + (step * direction)
+                var previousValue = value
 
-                $stepper.attr('data-value', value).trigger('change', value)
+                node.set(Math.max(0, value + step * direction))
+
+                if (value != previousValue) {
+                    notifyAgent()
+                }
             }
 
             function delay(amount) {
@@ -73,8 +91,6 @@
                 }, amount)
             }
 
-            $button.addClass('active')
-
             $document.one('mouseup touchend', function(e) {
                 e.preventDefault()
 
@@ -82,29 +98,16 @@
 
                 clearTimeout(timer)
 
+                // Handle regular clicks
                 if (Date.now() - startTime  < 250) {
                     adjust()
                 }
             })
 
+            $button.addClass('active')
+
+            // Delay 0.5s before button is considered held down
             delay(500)
-        })
-    })
-
-    // Bound stepper
-    $('.stepper[data-path]').each(function() {
-        var $stepper = $(this)
-
-        var path = $stepper.attr('data-path'),
-            node = device.child(path)
-
-        node.on('value', function(snapshot) {
-            $stepper.attr('data-value', snapshot.val() || 0)
-        })
-
-        $stepper.on('change', function(e, value) {
-            node.set(value)
-            notifyAgent()
         })
     })
 
@@ -115,26 +118,54 @@
             $minutes = $timer.find('.minutes .value')
 
         var path = $timer.attr('data-path'),
-            node = device.child(path)
+            node = device.child(path),
+            alarm = $timer.find('.alarm-sound')[0],
+            duration = 0,
+            startTime,
+            updateTimer
 
-        node.child('duration').on('value', function(snapshot) {
-            var seconds = snapshot.val() || 0,
+        function update() {
+            var diff = startTime ? Date.now() - startTime : 0,
+                seconds = Math.max(0, duration - (diff / 1000)),
                 hours = Math.floor(seconds / 3600),
-                minutes = Math.floor(seconds % 3600 / 60)
+                minutes = Math.ceil(seconds % 3600 / 60),
+                active = startTime > 0 && seconds > 0,
+                expired = startTime > 0 && seconds === 0
+
+            // console.log('Updating timer, startTime: %d, duration: %d, seconds: %d, diff: %d', startTime, duration, seconds, diff)
 
             $hours.text(sprintf('%02d', hours))
             $minutes.text(sprintf('%02d', minutes))
+            $timer.toggleClass('active', active).toggleClass('expired', expired)
+
+            clearTimeout(updateTimer)
+
+            if (active) {
+                updateTimer = setTimeout(update, 1000 - diff % 1000)
+            }
+
+            // Update alarm
+            expired ? alarm.play() : alarm.pause()
+
+            // TODO: Notify device to stop when timer expires
+        }
+
+        node.child('duration').on('value', function(snapshot) {
+            duration = snapshot.val() || 0
+
+            update()
         })
 
         node.child('start').on('value', function(snapshot) {
-            $timer.toggleClass('active', !!snapshot.val())
+            startTime = snapshot.val()
+
+            update()
         })
 
         $timer.find('.toggle').on('click', function() {
             var start = node.child('start')
             start.once('value', function(snapshot) {
                 start.set(snapshot.val() ? null : Date.now())
-                notifyAgent()
             })
         })
     })
